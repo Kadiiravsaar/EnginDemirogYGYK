ca499ecf389650f552d7a7dcecb251e5cc7530ca
kadiiravsaar <kadiiravsar@gmail.com>
2023-04-06 08:45:04 +0300

    GercekMusteri added TuzelMusteri added CustomerManager added Musteri added

---
+++ KampIntro/CustomerManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KampIntro
+{
+    public class CustomerManager
+    {
+        public void Add(Musteri musteri)
+        {
+            Console.WriteLine(musteri + "   eklendi.");
+
+        }
+    }
+}
--- KampIntro/Program.cs
+++ KampIntro/Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using KampIntro;
+
 Console.WriteLine("Hello, World!");
+
+
+GercekMusteri gercekMusteri = new GercekMusteri();
+gercekMusteri.ID = 1;
+gercekMusteri.TcNo = "606060";
+gercekMusteri.Adi = "Kadir";
+gercekMusteri.Soyadi = "Avşar";
+
+TuzelMusteri tuzelMusteri = new TuzelMusteri();
+tuzelMusteri.ID = 1;
+tuzelMusteri.MusteriNo = "61616161";
+tuzelMusteri.SirketAdi = "Kadoa";
+tuzelMusteri.VergiNo = "626262";
+
+Musteri musteri = new GercekMusteri();
+Musteri musteri2 = new TuzelMusteri();
+
+
+CustomerManager customerManager = new CustomerManager();
+customerManager.Add(gercekMusteri);
+customerManager.Add(tuzelMusteri);
+customerManager.Add(musteri);
+customerManager.Add(musteri2);
