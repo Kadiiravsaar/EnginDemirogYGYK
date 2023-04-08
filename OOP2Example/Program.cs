6c3a156da9a9ba1d1a7b41582809bccbb3538fc9
kadiiravsaar <kadiiravsar@gmail.com>
2023-04-08 08:09:24 +0300

    SendSmsService Added

--- OOP2Example/Program.cs
+++ OOP2Example/Program.cs
@@ -1,16 +1,21 @@
 ﻿using OOP2Example;
 
-ILoggerService databaseLogger = new DataBaseLoggerService();
+ILoggerService databaseLoggerInterface = new DataBaseLoggerService();
+DataBaseLoggerService databaseLogger = new DataBaseLoggerService();
 ILoggerService fileLogger = new FileLoggerService();
 // ExcelLogger excelLogger = new ExcelLogger(); // bu da (excelLogger) eklenirse aşağıda listeye(, new ExcelLogger() yerine) çalışır
 
 LoggerManager loggerManager = new LoggerManager();
-loggerManager.Logger(fileLogger);
+
+loggerManager.Logger(new DataBaseLoggerService()); // 3'üde aynı şeydir(10-11-12 satır)
 loggerManager.Logger(databaseLogger);
+loggerManager.Logger(databaseLoggerInterface);
+
+loggerManager.Logger(fileLogger);
 
 Console.WriteLine();
 
-List<ILoggerService> loggerManagers = new List<ILoggerService>() { databaseLogger,fileLogger, new ExcelLoggerService() };
+List<ILoggerService> loggerManagers = new List<ILoggerService>() { databaseLoggerInterface, databaseLogger,fileLogger, new ExcelLoggerService() };
 
 LoggerManager loggerManager1 = new LoggerManager();
 loggerManager1.LoggerList(loggerManagers);
